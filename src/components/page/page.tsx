2abe03a435ff2c1c520ddd2af1be03ad732b12b2
Kristina Garkovich <panda.bell.fran@gmail.com>
2023-02-25 20:17:22 +0100

    create todo app

---
+++ src/components/page/page.tsx
@@ -0,0 +1,10 @@
+export const TodoPage = () => (
+    <div className='container'>
+        <div className='page-content'>
+            <h1>TODO</h1>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+);
